e6c73e309ceb0269cf6ba2ba57fbf31d62a2ae33
Alexveryanat0r <sashapozdyshevvv@mail.ru>
2023-12-07 09:31:44 +0300

    Text styles is ready.

---
+++ app/src/main/java/com/example/myapplication/lama/DefaultPrompts.java
@@ -0,0 +1,5 @@
+package com.example.myapplication.lama;
+
+public class DefaultPrompts {
+    public static StaticPrompt Classic = new StaticPrompt("Below is an instruction that describes a task. Write a response that appropriately completes the request.");;
+}
